ef02d48b1a06b3931633f3f58e96950b965664dd
Julien Taiwa <taiwa.julien@gmail.com>
2023-03-09 20:54:44 +0100

    add changes

--- app.py
+++ app.py
@@ -408,5 +408,5 @@ dict = {
 #     return predicted_sentence
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
